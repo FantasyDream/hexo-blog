814b890479f692713d586f96e19dbae304c6c004
FantasyDream <S771247955@gmail.com>
2019-03-14 16:13:49 +0800

    Update open.js

--- scripts/open.js
+++ scripts/open.js
@@ -1,6 +1,7 @@
-var spawn = require('child_process').exec;
+var exec = require('child_process').exec;
 
 // Hexo 3 复制这段
 hexo.on('new', function(data){
-  spawn('start "" "C:\\Program Files\\Typora\\Typora.exe " ' + data.path);
+  let path = data.path.replace("/mnt/c","");
+  exec("typora.exe "+ path);
 });
